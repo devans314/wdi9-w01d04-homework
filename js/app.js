bcb63a70225cd14c988dab2e3bd36d1c2027956c
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-09-13 20:46:49 -0600

    Commit 6 - Prime Numbers.

--- js/app.js
+++ js/app.js
@@ -92,12 +92,59 @@
 // console.log(sumArray([1, 2, 3, 4, 5, 6]));
 // => 21
 
-const sumArray = (arr) => {
-    let sum = 0;
-    for(i = 0; i < arr.length; i++){
-        sum += arr[i];
+// const sumArray = (arr) => {
+//     let sum = 0;
+//     for(i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     console.log(sum);
+// }
+//     sumArray([5, 10, 10, 5]);
+
+// 6. Prime Numbers
+// A Prime number is a number that is not evenly divisible by another number except 
+// 1 and itself. If you want to read more deeply about it, go here. 
+//To test whether a number is Prime, you only need to test as far as 
+//the square root of that number. 
+//This is advisable for optimization and testing large numbers.
+
+// Step One
+// Write a function called checkPrime that will test whether a number is Prime. 
+//The function will return true (Boolean) if Prime, false if not. 
+//Hint: Check every number up to the square root. To do this, try a for loop.
+
+// Step Two
+// Write another function called printPrimes that will print (console log) all the 
+// Primes up to an arbitrary limit. For example, if you invoke your function 
+// with printPrimes(97), it will print all the Prime numbers up to and including 97. 
+// This function can call on the previous checkPrime function.
+
+const checkPrime = (num) => {
+    let prime = true;
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0) {
+            prime = false;
+            break;}
+        
     }
-    console.log(sum);
+    return prime && (num > 1);
 }
-    sumArray([5, 10, 10, 5]);
+
+const printPrimes = (num) => {
+    let newPrime = checkPrime(num);
+    console.log(newPrime);
+    let primeArray = [];
+    if(newPrime === true){
+        for(i = 1; i <= num; i++){
+            if(i % 1 === 0){
+                primeArray.push(i);
+            } 
+        } 
+    } else {
+        console.log("not prime");
+    }
+  console.log(primeArray); 
+}
+printPrimes(97);
+
 
